eda127989a64aa4efd373de43263d9649186cc94
larrytabeh <larrytabeh@gmail.com>
2023-01-26 19:58:16 +0100

    Added check for update button

--- lib/app_screens/error_screen.dart
+++ lib/app_screens/error_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:xperiencebase/constants/colors.dart';
 import 'package:xperiencebase/widgets_functions/widgets/appbar.dart';
-
+import 'package:xperiencebase/widgets_functions/widgets/button.dart';
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:xperiencebase/widgets_functions/widgets/padding.dart';
+import 'package:xperiencebase/widgets_functions/functions/snackbar_menus.dart';
 
 class ErrorScreen extends StatefulWidget {
   const ErrorScreen({Key? key}) : super(key: key);
@@ -22,6 +25,7 @@ class _ErrorScreenState extends State<ErrorScreen> {
             top: 100.0,
             child: Center(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   SizedBox(
                     height: 180.0,
@@ -32,6 +36,27 @@ class _ErrorScreenState extends State<ErrorScreen> {
                     'Oops! It seems like your app is out of date or you are offline, please try updating your app or connecting to active internet connection. Close all instances of the app and then reload',
                     style: TextStyle(color: kRedColor),
                     textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 150.0,
+                  ),
+                  RoundedTextButton(
+                    text: 'Check for update',
+                    buttonColor: kPrimaryColor,
+                    onPressed: () async {
+                      const url = 'https://play.google.com/store/apps/details?id=com.xperiencebase';
+                      if (!await launchUrl(Uri.parse(url),
+                          mode: LaunchMode.externalApplication)) {
+                        showToast(
+                            title: 'Failure',
+                            body:
+                                'Please visit Google play to check for update.',
+                            snackBarType: ContentType.failure,
+                            context: context);
+                      } else {
+                        //Do something here if launch is successful
+                      }
+                    },
                   )
                 ],
               ),
